50b25d4a416a1b478d04ba0bc6d401553abff1ec
kevin <agonyice0115@gmail.com>
2017-06-14 17:25:57 +0800

    change target to es6

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,6 @@ let gulp = require("gulp");
 let ts = require("gulp-typescript");
 let tsProject = ts.createProject("tsconfig.json");
 
-//gulp create dir
-gulp.task("create dist dir", function () {
-  gulp.src()
-    .pipe(gulp.dest('./build/templates'))
-    .pipe(gulp.dest('./build/minified_templates'));
-});
-
 // gulp build and create dir
 gulp.task("build-code", function () {
   return tsProject.src()
