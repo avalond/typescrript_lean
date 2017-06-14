2b8d99f17e4c2e6ce15b17bf1ebf22b7afe3911d
kevin <agonyice0115@gmail.com>
2017-06-14 17:34:36 +0800

    change target to es6

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let gulp = require("gulp");
 let ts = require("gulp-typescript");
 let tsProject = ts.createProject("tsconfig.json");
 
-// gulp build and create dir
+// gulp build and create dirs
 gulp.task("build-code", function () {
   return tsProject.src()
     .pipe(tsProject())
